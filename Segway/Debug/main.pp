0529c1c10bd425ae1cfffd7aa9cd46f8cdf3acfc
ngrajales1 <ngrajales1.uh@gmail.com>
2016-06-12 21:12:34 -0500

    all the other files
    hopefully this works

---
+++ Segway/Debug/main.pp
@@ -0,0 +1,59 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_ints.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_types.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/debug.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/interrupt.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/pin_map.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/rom.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/uart.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/utils/uartstdio.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/sensorlib/hw_mpu9150.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/sensorlib/hw_ak8975.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/sensorlib/i2cm_drv.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/sensorlib/ak8975.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/sensorlib/mpu9150.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/sensorlib/comp_dcm.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/examples/boards/ek-tm4c123gxl-boostxl-senshub/drivers/rgb.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/pwm.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_gpio.h
+
+../main.c: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_ints.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/utils/uartstdio.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/sensorlib/hw_mpu9150.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/sensorlib/hw_ak8975.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/sensorlib/i2cm_drv.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/sensorlib/ak8975.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/sensorlib/mpu9150.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/sensorlib/comp_dcm.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/examples/boards/ek-tm4c123gxl-boostxl-senshub/drivers/rgb.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/pwm.h: 
+C:/ti/TivaWare_C_Series-2.1.2.111/inc/hw_gpio.h: 
